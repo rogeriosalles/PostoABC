cced0c509147f63b36adb3ac55db1b2e3581265a
rogeriosalles <139908268+rogeriosalles@users.noreply.github.com>
2023-11-17 05:17:37 -0300

    Atualização do projeto

--- PostoABC.dpr
+++ PostoABC.dpr
@@ -4,10 +4,10 @@ uses
   Vcl.Forms,
   uEmpresa in 'Model\uEmpresa.pas',
   uCombustivel in 'Model\uCombustivel.pas',
-  uBombaCombustivel in 'Model\uBombaCombustivel.pas',
+  uBombaAbastecimento in 'Model\uBombaAbastecimento.pas',
   uTanqueCombustivel in 'Model\uTanqueCombustivel.pas',
   uVenda in 'Model\uVenda.pas',
-  UnitBombaAbastecimento in 'View\UnitBombaAbastecimento.pas' {Form1},
+  UnitBombaAbastecimento in 'View\UnitBombaAbastecimento.pas' {frmBombaAbastecimento},
   UnitCombustivel in 'View\UnitCombustivel.pas' {frmCombustive},
   UnitEmpresa in 'View\UnitEmpresa.pas' {frmEmpresa},
   UnitPrincipal in 'View\UnitPrincipal.pas' {frmPrincipal},
@@ -21,7 +21,12 @@ uses
   uDAOCombustivel in 'DAO\uDAOCombustivel.pas',
   uControllerCombustivel in 'Controller\uControllerCombustivel.pas',
   uDAOTanqueCombustivel in 'DAO\uDAOTanqueCombustivel.pas',
-  uControllerTanqueCombustivel in 'Controller\uControllerTanqueCombustivel.pas';
+  uControllerTanqueCombustivel in 'Controller\uControllerTanqueCombustivel.pas' {$R *.res},
+  uControllerBombaAbastecimento in 'Controller\uControllerBombaAbastecimento.pas',
+  uDAOBombaAbastecimento in 'DAO\uDAOBombaAbastecimento.pas',
+  uDAOVenda in 'DAO\uDAOVenda.pas',
+  uControllerVenda in 'Controller\uControllerVenda.pas',
+  UnitRelatorio in 'View\UnitRelatorio.pas' {frmRelatorio};
 
 {$R *.res}
 
@@ -29,5 +34,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.CreateForm(TfrmRelatorio, frmRelatorio);
   Application.Run;
 end.
